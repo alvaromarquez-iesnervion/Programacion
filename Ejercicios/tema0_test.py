fff6962967c52e75468ce185a9d53ff14bcc54ef
Alvaro Marquez Cobos <alvaro.marquez@iesnervion.es>
2025-09-23 18:48:23 +0200

    Ejercicios completados

--- Ejercicios/tema0_test.py
+++ Ejercicios/tema0_test.py
@@ -1,11 +1,47 @@
 from tema0 import *
 
-print("TEST EJERCICIO 1:/n")
+def test_numbers_between():
+    limpiar_consola()
+    number1= int(input("Enter your first number "))
+    number2= int(input("Enter your first number "))
 
-number = 7
-odd_even(number)
+    numbers_between(number1, number2)
 
-print("/nTEST EJERCICIO 2:/n")
+def test_max_of_two():
+    limpiar_consola()
+    number1= int(input("Enter your first number "))
+    number2= int(input("Enter your first number "))
 
+    print("The max is:", max_of_two(number1, number2))
 
- 
+def test_is_vowel():
+    limpiar_consola()
+    char= input("Enter a letter ")
+
+    if is_vowel(char):
+        print("Your letter is a vowel")
+    else:
+        print("your letter is a consonant")
+
+def test_calculator():
+    limpiar_consola()
+    number1= float(input("Enter your first number "))
+    number2= float(input("Enter your first number "))
+    operation=int(input("Enter a number between 1 and 4\n1.addition\n2.subtraction\n3.multiplication\n4.division\n"))
+
+    print("The result is:", calculator(number1, number2, operation))
+
+#descomentar para probar la función
+
+#odd_even()
+#order_numbers() 
+#sum_num() 
+#secret_code() 
+#count_learning() 
+#factorial() 
+#prime_number() 
+#triangle() 
+#test_numbers_between() 
+#test_max_of_two()
+#test_is_vowel()
+test_calculator()
